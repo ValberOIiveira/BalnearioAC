4191fe8efd48975583b7cb816b2ee3667d216414
RODRIGO TERRA COSTA <rodrigoterra@dgtbr.com.br>
2025-04-09 16:43:29 -0400

    fix: Criando models visitante

---
+++ BalnearioAC/Models/Visitor.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BalnearioAC.Models
+{
+    [Table("visitors")]
+    public class Visitor
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("cpf")]
+        public string Cpf { get; set; }
+
+        [Column("age")]
+        public DateTime Age { get; set; }
+
+        [Column("id_user")]
+        public int IdUser { get; set; }
+
+    }
+}
