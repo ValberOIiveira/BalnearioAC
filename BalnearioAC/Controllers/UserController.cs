658045c8eedb4a982cdc704913df3ec78daf752a
RhyanSKomm <rhyankomm@hotmail.com>
2025-04-11 14:53:59 -0400

    fix: enviando controller de usuário

--- BalnearioAC/Controllers/UserController.cs
+++ BalnearioAC/Controllers/UserController.cs
@@ -2,11 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-using Microsoft.EntityFrameworkCore;
 using BalnearioAC.Database;
 using BalnearioAC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BalnearioAC.Controllers
 {
@@ -22,46 +21,66 @@ namespace BalnearioAC.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<User>> Get() 
+        public async Task<IEnumerable<User>> getUser()
         {
             return await _context.Users.ToListAsync();
         }
 
         [HttpPost]
-        public async Task<ActionResult<User>> Post(User user) 
+        public async Task<IActionResult> PostUser([FromBody] User user)
         {
+            if (user == null)
+            {
+                return BadRequest("Usuário não pode ser nulo");
+            }
+
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return user;
+
+            return user.Id > 0 ? Ok(user) : BadRequest("Erro ao cadastrar usuário");
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult<User>> Put(int id, [FromBody] User user)
-        { 
-            var existente = await _context.Users.FindAsync(id); 
-            if (existente == null) return NotFound();   
+        public async Task<IActionResult> PutUser(int id, [FromBody] User user)
+        {
+            if (user == null || id != user.Id)
+            {
+                return BadRequest("Usuário não pode ser nulo ou ID inválido");
+            }
 
-            existente.Name = user.Name;
-            existente.Cpf = user.Cpf;
-            existente.Email = user.Email;
-            existente.Phone = user.Phone;
-            existente.Age = user.Age;
-            existente.Passwd = user.Passwd;
-            existente.Id_user_type = user.Id_user_type;
+            var existingUser = await _context.Users.FindAsync(id);
+            if (existingUser == null)
+            {
+                return NotFound("Usuário não encontrado");
+            }
+
+            existingUser.Name = user.Name;
+            existingUser.Cpf = user.Cpf;
+            existingUser.Email = user.Email;
+            existingUser.Phone = user.Phone;
+            existingUser.Age = user.Age;
+            existingUser.Passwd = user.Passwd;
+            existingUser.Id_user_type = user.Id_user_type;
+
             await _context.SaveChangesAsync();
-            return existente;
+
+            return Ok(existingUser);
 
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult<User>> Delete(int id) 
+        public async Task<IActionResult> DeleteUser(int id)
         {
-            var existente = await _context.Users.FindAsync(id);
-            if (existente == null) return NotFound();
-            _context.Users.Remove(existente);
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound("Usuário não encontrado");
+            }
+
+            _context.Users.Remove(user);
             await _context.SaveChangesAsync();
-            return existente;
+
+            return Ok("Usuário excluído com sucesso");
         }
-        
     }
 }
